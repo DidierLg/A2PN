8320399dd1058630ea5ee8cf2c927fc8f0b935ce
DidierLg <30965675+DidierLg@users.noreply.github.com>
2021-06-15 08:56:28 -0400

    Update main.js

--- main.js
+++ main.js
@@ -15746,12 +15746,12 @@ var TBinaryData = {
    ,HandleReleased$:function($){return $.ClassType.HandleReleased($)}
 };
 TBinaryData.$Intf={
-   IBinaryDataWriteAccess:[TBinaryData.AppendBytes,TBinaryData.AppendStr,TBinaryData.AppendMemory,TBinaryData.AppendBuffer,TBinaryData.AppendFloat32,TBinaryData.AppendFloat64,TBinaryData.Write$4,TBinaryData.WriteFloat32,TBinaryData.WriteFloat64,TBinaryData.CopyFrom$2,TBinaryData.CopyFromMemory,TBinaryData.CutBinaryData,TBinaryData.CutStream,TBinaryData.CutTypedArray]
-   ,IBinaryDataExport:[TBinaryData.ToBase64,TBinaryData.ToString$10,TBinaryData.ToTypedArray,TBinaryData.ToBytes,TBinaryData.ToHexDump,TBinaryData.ToStream,TBinaryData.Clone$1]
+   IBinaryDataBitAccess:[TBinaryData.GetBitCount,TBinaryData.GetBit$1,TBinaryData.SetBit$1]
    ,IBinaryDataReadAccess:[TBinaryData.ReadFloat32,TBinaryData.ReadFloat64,TBinaryData.ReadBool,TBinaryData.ReadInt,TBinaryData.ReadStr$1,TBinaryData.ReadBytes]
-   ,IBinaryDataImport:[TBinaryData.FromBase64]
+   ,IBinaryDataWriteAccess:[TBinaryData.AppendBytes,TBinaryData.AppendStr,TBinaryData.AppendMemory,TBinaryData.AppendBuffer,TBinaryData.AppendFloat32,TBinaryData.AppendFloat64,TBinaryData.Write$4,TBinaryData.WriteFloat32,TBinaryData.WriteFloat64,TBinaryData.CopyFrom$2,TBinaryData.CopyFromMemory,TBinaryData.CutBinaryData,TBinaryData.CutStream,TBinaryData.CutTypedArray]
    ,IBinaryDataReadWriteAccess:[TBinaryData.ReadFloat32,TBinaryData.ReadFloat64,TBinaryData.ReadBool,TBinaryData.ReadInt,TBinaryData.ReadStr$1,TBinaryData.ReadBytes,TBinaryData.AppendBytes,TBinaryData.AppendStr,TBinaryData.AppendMemory,TBinaryData.AppendBuffer,TBinaryData.AppendFloat32,TBinaryData.AppendFloat64,TBinaryData.Write$4,TBinaryData.WriteFloat32,TBinaryData.WriteFloat64,TBinaryData.CopyFrom$2,TBinaryData.CopyFromMemory,TBinaryData.CutBinaryData,TBinaryData.CutStream,TBinaryData.CutTypedArray]
-   ,IBinaryDataBitAccess:[TBinaryData.GetBitCount,TBinaryData.GetBit$1,TBinaryData.SetBit$1]
+   ,IBinaryDataExport:[TBinaryData.ToBase64,TBinaryData.ToString$10,TBinaryData.ToTypedArray,TBinaryData.ToBytes,TBinaryData.ToHexDump,TBinaryData.ToStream,TBinaryData.Clone$1]
+   ,IBinaryDataImport:[TBinaryData.FromBase64]
    ,IBinaryTransport:[TAllocation.DataOffset$1,TAllocation.DataGetSize$1,TAllocation.DataRead$1,TAllocation.DataWrite$1]
    ,IAllocation:[TAllocation.GetHandle,TAllocation.GetTotalSize,TAllocation.GetSize$3,TAllocation.GetTransport,TAllocation.Allocate$1,TAllocation.Grow,TAllocation.Shrink,TAllocation.ReAllocate,TAllocation.Transport,TAllocation.Release$2]
 }
@@ -17303,8 +17303,8 @@ var TFProgramme = {
          if (Trim$_String_(TFProgramme.PartieCSV(Self,s,3,";"))!="") {
             couleur = Trim$_String_(TFProgramme.PartieCSV(Self,s,3,";"));
          }
-         s = StrReplace(s,"<coul>","<span id=\""+couleur+"\">");
-         s = StrReplace(s,"<\/coul>","<\/span>");
+         s = StrReplace(s,"<coul>","<b><span id=\""+couleur+"\">");
+         s = StrReplace(s,"<\/coul>","<\/span><\/b>");
          if (Trim$_String_(TFProgramme.PartieCSV(Self,s,3,";"))!="") {
             urgence = Trim$_String_(TFProgramme.PartieCSV(Self,s,1,";"));
             intro = TFProgramme.PartieCSV(Self,s,2,";");
@@ -17398,7 +17398,7 @@ var TFProgramme = {
          TLayout.Resize$7$(Self.FLayoutProg,Self);
       }
       w$2 = TW3MovableControl.GetLeft(Self.imgTheme)+TW3MovableControl.GetWidth$(Self.imgTheme)-TW3MovableControl.GetLeft(Self.imgProg)-10;
-      h$3 = $Div(TW3MovableControl.GetHeight$(Self.scroll$1),2);
+      h$3 = Trunc(TW3MovableControl.GetHeight$(Self.scroll$1)/3*2);
       TW3MovableControl.SetWidth$(Self.lboxTheme,w$2);
       TW3MovableControl.SetHeight$(Self.lboxTheme,h$3);
       TW3MovableControl.SetLeft$(Self.lboxTheme,(TW3MovableControl.GetLeft(Self.imgTheme)+TW3MovableControl.GetWidth$(Self.imgTheme)-w$2));
@@ -17531,10 +17531,10 @@ var TFProgramme = {
       TW3TagContainer.SetComponentName(Self.imgTheme,"imgTheme");
       TW3CustomControl._setMouseClick(Self.imgTheme,$Event1(Self,TFProgramme.imgThemeClick));
       Self.lboxTheme = TW3TagContainer.Create$81$($New(TW3ListBox),Self);
-      TW3MovableControl.SetWidth$(Self.lboxTheme,296);
+      TW3MovableControl.SetWidth$(Self.lboxTheme,368);
       TW3MovableControl.SetVisible(Self.lboxTheme,false);
-      TW3MovableControl.SetTop$(Self.lboxTheme,200);
-      TW3MovableControl.SetLeft$(Self.lboxTheme,96);
+      TW3MovableControl.SetTop$(Self.lboxTheme,112);
+      TW3MovableControl.SetLeft$(Self.lboxTheme,56);
       TW3MovableControl.SetHeight$(Self.lboxTheme,336);
       TW3TagContainer.SetComponentName(Self.lboxTheme,"lboxTheme");
       TW3ListBox.SetOnItemClick(Self.lboxTheme,$Event2(Self,TFProgramme.lboxThemeItemClick));
